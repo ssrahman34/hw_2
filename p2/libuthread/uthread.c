7d1d08567bc2a56e351edec38f7928795523c1a5
Sarah <sarah.gateway@yahoo.com>
2019-02-04 12:50:56 -0800

    create_thread

--- p2/libuthread/uthread.c
+++ p2/libuthread/uthread.c
@@ -25,7 +25,13 @@ uthread_t uthread_self(void)
 
 int uthread_create(uthread_func_t func, void *arg)
 {
-	/* TODO Phase 2 */
+	ucontext_t *thread;
+	void *top_of_stack = uthread_ctx_alloc_stack();
+	int retVal = uthread_ctx_init(thread, top_of_stack, func, arg);
+	if (retVal== -1){
+		return -1;
+	}
+	return uthread_self(); //return TID
 }
 
 void uthread_exit(int retval)
