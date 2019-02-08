126911302c01e2fbf42ff8d2dd135d913ec844e8
Iyesha Puri <ipuri@ucdavis.edu>
2019-02-07 17:15:15 -0800

    clean

--- p2/libuthread/queue.c
+++ p2/libuthread/queue.c
@@ -1,144 +1,123 @@
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#include <stdint.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <stdio.h> 
 #include "queue.h"
 
-struct Node{
-	void* key;
-	struct Node *next; //pointer to next node
+struct Node {
+    void * key;
+    struct Node * next; //pointer to next node
 };
 
 struct queue {
-	struct Node *front, *rear;
+    struct Node * front, * rear;
 };
 
-queue_t queue_create(void)
-{
-	
-	struct queue* queue = (struct queue*) malloc(sizeof(struct queue));
-	queue->front = NULL;
-	queue->rear = NULL;  
-	return queue;
+queue_t queue_create(void) {
+    struct queue * queue = (struct queue * ) malloc(sizeof(struct queue));
+    queue - > front = NULL;
+    queue - > rear = NULL;
+    return queue;
 }
 
-int queue_destroy(queue_t queue)
-{
-	if(queue == NULL || queue_length(queue) != 0){
-		return -1;
-	}
-	struct Node* curr= queue->front;
-	struct Node* next;
-	while(curr != NULL){
-		next = curr->next;
-		free(curr);
-		curr = next;
-	}//free the nodes in the quque
-	free(queue);
-	return 0;
+int queue_destroy(queue_t queue) {
+    if (queue == NULL || queue_length(queue) != 0) {
+        return -1;
+    }
+    struct Node * curr = queue - > front;
+    struct Node * next;
+    while (curr != NULL) {
+        next = curr - > next;
+        free(curr);
+        curr = next;
+    } //free the nodes in the quque
+    free(queue);
+    return 0;
 }
 
-int queue_enqueue(queue_t queue, void *data)
-{
-	if(data == NULL || queue== NULL){
-		return -1;
-	}
-	struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
-	new_node->key = data;
-	new_node->next = NULL;
-	//struct Node *temp = (struct Node*) malloc(sizeof(struct Node));
-	//temp = data;//this might be a problem //might need to make key a *
-	if (queue->rear == NULL){
-		queue->front = queue->rear = new_node;		
-		return 0;
-	} 
-	queue->rear->next = new_node;
-	queue->rear= new_node;
+int queue_enqueue(queue_t queue, void * data) {
+    if (data == NULL || queue == NULL) {
+        return -1;
+    }
+    struct Node * new_node = (struct Node * ) malloc(sizeof(struct Node));
+    new_node - > key = data;
+    new_node - > next = NULL;
+
+    if (queue - > rear == NULL) {
+        queue - > front = queue - > rear = new_node;
+        return 0;
+    }
+    queue - > rear - > next = new_node;
+    queue - > rear = new_node;
     return 0;
 
 }
 
-int queue_dequeue(queue_t queue, void **data)
-{
-	if(queue == NULL || data == NULL || queue_length(queue) == 0 || queue->front == NULL){
-		return -1;
-	}
-	
-	*data = &(queue->front->key);
-	queue->front = queue->front->next;
-	if(queue->front == NULL){
-		queue->rear=NULL;
-	}
-	//free (queue->front);
-	return 0;
+int queue_dequeue(queue_t queue, void ** data) {
+    if (queue == NULL || data == NULL || queue_length(queue) == 0 || queue - > front == NULL) {
+        return -1;
+    }
+
+    * data = & (queue - > front - > key);
+    queue - > front = queue - > front - > next;
+    if (queue - > front == NULL) {
+        queue - > rear = NULL;
+    }
+    //free (queue->front);
+    return 0;
 }
 
-int queue_delete(queue_t queue, void *data)
-{
-	if(queue == NULL || data == NULL){
-		return -1;
-	}
-	struct Node *curr = queue->front;
-	while(curr->next != NULL){
-		if(curr->next->key == data){
-			struct Node *temp = curr->next;
-			curr->next = curr->next->next;
-			free(temp); //delete
-			return 0;
-		}
-		curr = curr->next;
-	}
-	return -1; //data was not found in queue
+int queue_delete(queue_t queue, void * data) {
+    if (queue == NULL || data == NULL) {
+        return -1;
+    }
+    struct Node * curr = queue - > front;
+    while (curr - > next != NULL) {
+        if (curr - > next - > key == data) {
+            struct Node * temp = curr - > next;
+            curr - > next = curr - > next - > next;
+            free(temp); //delete
+            return 0;
+        }
+        curr = curr - > next;
+    }
+    return -1; //data was not found in queue
 }
 
-int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
-{
-	if(queue==NULL || func == NULL){
-		return -1;
-	}
-	struct Node *curr = queue->front;
-	//void (*ptr)() = &func; //this is the function to call
-	int retVal;
-	while(curr!= NULL){
-		retVal = func(curr, arg); //the result of the function call???
-		// Do we need to iterate through data? is is *data in func
-		if(retVal == 1){
-			if(data == NULL){
-				return -1;
-			}
-			else{
-			//	data = curr;//data received the data item where iteration stoped
-				break;
-			}
-		}
-		curr=curr->next;
-	}
-	return 0;
+int queue_iterate(queue_t queue, queue_func_t func, void * arg, void ** data) {
+    if (queue == NULL || func == NULL) {
+        return -1;
+    }
+    struct Node * curr = queue - > front;
+    //void (*ptr)() = &func; //this is the function to call
+    int retVal;
+    while (curr != NULL) {
+        retVal = func(curr, arg); //the result of the function call???
+        // Do we need to iterate through data? is is *data in func
+        if (retVal == 1) {
+            if (data == NULL) {
+                return -1;
+            } else {
+                break;
+            }
+        }
+        curr = curr - > next;
+    }
+    return 0;
 }
 
-int queue_length(queue_t queue)
-{
-	if(queue == NULL){
-		return -1;
-	}
-	if(queue->front == NULL){
-		return 0; //we have an empty queue
-	}
-	int length = 1;
-	struct Node* curr = queue->front;
-	while(curr != queue->rear){
-		length++;
-		curr = curr->next;
-	}
-	//length = count(queue->front);
-	return length;
+int queue_length(queue_t queue) {
+    if (queue == NULL) {
+        return -1;
+    }
+    if (queue - > front == NULL) {
+        return 0; //we have an empty queue
+    }
+    int length = 1;
+    struct Node * curr = queue - > front;
+    while (curr != queue - > rear) {
+        length++;
+        curr = curr - > next;
+    }
+    return length;
 }
-
-/*int count(struct Node* head){
- if (head == NULL) 
-        return 0; 
-  
-    // count is 1 + count of remaining list 
-    return 1 + count(head->next); 
-}*/
-
--- p2/libuthread/uthread.c
+++ p2/libuthread/uthread.c
@@ -1,9 +1,9 @@
-#include <assert.h>
-#include <signal.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <assert.h> 
+#include <signal.h> 
+#include <stddef.h> 
+#include <stdint.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <sys/time.h>
 
 #include "context.c"
@@ -16,276 +16,205 @@
 #include "queue.c"
 
 int num_threads = 0;
-struct queue* q;
-struct queue* blocked_queue;
-struct queue* zombie_queue;
+struct queue * q;
+struct queue * blocked_queue;
+struct queue * zombie_queue;
 
-struct thread* running_thread;
+struct thread * running_thread;
 
-typedef enum{
-        Blocked ,
-        Running ,
-        Ready ,
-        Zombie 
-}State;
+typedef enum {
+    Blocked,
+    Running,
+    Ready,
+    Zombie
+}
+State;
 
-typedef struct thread{
-	uthread_t TID;
-	uthread_t join_tid;
-	bool is_joined;
-	State state;
-	int retVal; //so we can use in jion
-	uthread_ctx_t *context;
-	void* arg;
-	uthread_func_t func;
-	int* stack;// = uthread_ctx_alloc_stack();
-}thread;
+typedef struct thread {
+    uthread_t TID;
+    uthread_t join_tid;
+    bool is_joined;
+    State state;
+    int retVal; //so we can use in jion
+    uthread_ctx_t * context;
+    void * arg;
+    uthread_func_t func;
+    int * stack; // = uthread_ctx_alloc_stack();
+}
+thread;
 
-int main_thread(uthread_func_t func, void *arg);
+int main_thread(uthread_func_t func, void * arg);
 
 /* Look for the next available thread */
-void uthread_yield(void)
-{
-	if(queue_length(q) > 0){
-		struct thread *running = running_thread;
-		//printf("%s", temp->TID);
-		struct Node *top = q->front;
-		struct thread* temp = (struct thread*)top->key;
-//		printf("%ld\n", temp);
-		/*if(temp->TID == 0){
-			printf("NULL");
-		}else{
+void uthread_yield(void) {
+    if (queue_length(q) > 0) {
+        struct thread * running = running_thread;
+        struct Node * top = q -> front;
+        struct thread * temp = (struct thread * ) top -> key;
+        struct thread * curr = NULL; //= malloc(sizeof(thread));
+        //prempt disable?
+        queue_dequeue(q, (void ** ) & curr);
 
-			printf("front of queue TID is = %d\n", temp->TID);
-		}*/
-		struct thread *curr = NULL;//= malloc(sizeof(thread));
-                //prempt disable?
-
-		queue_dequeue(q,(void **)&curr);
-//		printf("%ld\n", curr);
-//		printf("curr  DEQUEUED tid : %d\n", curr->TID);
-		if(temp->state == Ready){
-
-                	if(running->state == Running){
-
-			running->state = Ready;
-                	queue_enqueue(q, (void*)running);
-			}
-		temp->state = Running;
-		running_thread = temp;
-		
-		uthread_ctx_switch(running->context, temp->context);
-		}
-		//uthread_ctx_switch(running->context, temp->context);
-	}
-	return;
+        if (running -> state == Running) {
+            running -> state = Ready;
+            queue_enqueue(q, (void * ) running);
+        }
+        temp -> state = Running;
+        running_thread = temp;
+        uthread_ctx_switch(running -> context, temp -> context);
+    }
+    return;
 }
 
-uthread_t uthread_self(void)
-{
-	return running_thread->TID;	
+uthread_t uthread_self(void) {
+    return running_thread -> TID;
 }
 
-int uthread_create(uthread_func_t func, void *arg)
-{
-	if(num_threads == 0){
-		main_thread(func, arg);
-	}
-	
-		struct thread *thr = malloc(sizeof(struct thread));
-		void *top_of_stack = uthread_ctx_alloc_stack();
-	
-		
-		thr->TID = num_threads;
-		thr->func = func;
-		thr->arg = arg;
-		thr->is_joined = false;
-		//thread->parent = running_thread;
-		thr->state = Ready; //assign to read?
-		thr->stack = top_of_stack; //assign stack
-		thr->context = malloc(sizeof(uthread_ctx_t));
-		int retVal = uthread_ctx_init(thr->context, top_of_stack, func, arg);
-		if (retVal != 0){
-			return -1;
-                }	
-		num_threads++; //increment thread number
-		queue_enqueue(q, (void*)thr); //add the struct to the queue
-		//struct Node *top = q->front;
-		//struct thread *temp = top->key;
-		//printf("temp TID %d", temp->TID);
-		return thr->TID;
-	
+int uthread_create(uthread_func_t func, void * arg) {
+    if (num_threads == 0) {
+        main_thread(func, arg);
+    }
+    struct thread * thr = malloc(sizeof(struct thread));
+    void * top_of_stack = uthread_ctx_alloc_stack();
+
+    thr -> TID = num_threads;
+    thr -> func = func;
+    thr -> arg = arg;
+    thr -> is_joined = false;
+    //thread->parent = running_thread;
+    thr -> state = Ready; //assign to read?
+    thr -> stack = top_of_stack; //assign stack
+    thr -> context = malloc(sizeof(uthread_ctx_t));
+    int retVal = uthread_ctx_init(thr -> context, top_of_stack, func, arg);
+    if (retVal != 0) {
+        return -1;
+    }
+    num_threads++; //increment thread number
+    queue_enqueue(q, (void * ) thr); //add the struct to the queue
+    return thr -> TID;
 }
 
-void uthread_exit(int retval)
-{
-	running_thread->state = Zombie;
-	running_thread->retVal = retval;
-	struct thread* running = running_thread;
-	uthread_t TID_to_unblock = running_thread->join_tid;
-	queue_enqueue(zombie_queue, running);
+void uthread_exit(int retval) {
+    running_thread -> state = Zombie;
+    running_thread -> retVal = retval;
+    struct thread * running = running_thread;
+    uthread_t TID_to_unblock = running_thread -> join_tid;
+    queue_enqueue(zombie_queue, running);
 
-	struct Node *curr_blocked = blocked_queue->front;
+    struct Node * curr_blocked = blocked_queue -> front;
 
-	while(curr_blocked != NULL){
-		struct thread* temp_blocked = curr_blocked->key;
-		if(temp_blocked->TID == TID_to_unblock){
-			temp_blocked->state = Ready;
-			queue_delete(blocked_queue, temp_blocked); //make it unblocked
-			queue_enqueue(q, temp_blocked);
-			break;
-		}//we found which one to unblocn!
-		curr_blocked = curr_blocked->next;
-	}
-	uthread_yield();
-/*	struct Node *top = q->front;
-        struct thread* temp = (struct thread*)top->key;
-	if(queue_length(q) > 0){
-		struct thread *curr;//= malloc(sizeof(thread));
-        	queue_dequeue(q,(void **)&curr);
-		printf("exit and switch:");
-		uthread_ctx_switch(running->context, temp->context); //what if there is nothing left in queue?
-	}
-	else{
-		printf("empty queue");
-		printf(" TID TO UNBLOCK: %d", TID_to_unblock);
-	}*/
-	//UPDATE your parent to be UNBLOCKEd
-	//add parent to the READY QUEUE
-	// pop the next ready item from the queue
-	// context switch to it..
+    while (curr_blocked != NULL) {
+        struct thread * temp_blocked = curr_blocked -> key;
+        if (temp_blocked -> TID == TID_to_unblock) {
+            temp_blocked -> state = Ready;
+            queue_delete(blocked_queue, temp_blocked); //make it unblocked
+            queue_enqueue(q, temp_blocked);
+            break;
+        } //we found which one to unblocn!
+        curr_blocked = curr_blocked -> next;
+    }
+    uthread_yield();
 }
 
+int uthread_join(uthread_t tid, int * retval) {
+    if (running_thread -> TID == tid) {
+        return -1;
+    } //if running thread is parent
 
-int uthread_join(uthread_t tid, int *retval)
-{
+    if (tid == 0) {
+        return -1;
+    }
 
-	if(running_thread->TID == tid){
-		return -1;
-	}//if running thread is parent
+    int found = 0;
+    int blocked = 0;
+    struct Node * curr_zombie = zombie_queue -> front;
+    struct Node * curr_q = q -> front;
+    struct Node * curr_blocked = blocked_queue -> front;
 
-	if(tid == 0){return -1;}
+    while (curr_q != NULL) {
+        struct thread * temp = curr_q -> key; //get the struct
+        if (temp -> TID == tid) {
+            found = 1;
+            if (temp -> is_joined == false) {
+                blocked = 1;
+                temp -> is_joined = true;
+                running_thread -> state = Blocked; //block the running thead.
+                queue_enqueue(blocked_queue, running_thread);
+                temp -> join_tid = running_thread -> TID; //temp will use join_tid to figure which parent to unblock
+                uthread_yield();
+                break;
+            } else {
+                return -1;
+            }
+        }
+        curr_q = curr_q -> next;
+    }
 
-	int found =0;
-	int blocked = 0;
-	struct Node *curr_zombie = zombie_queue->front;
-	struct Node *curr_q = q->front;
-	struct Node *curr_blocked = blocked_queue->front;
-	
-	while(curr_q != NULL){
-		struct thread* temp = curr_q->key; //get the struct
-		if(temp->TID == tid){
-			found = 1;
-			if(temp->is_joined == false){
-				blocked = 1;
-				temp->is_joined = true;
-				running_thread->state = Blocked; //block the running thead.
-				queue_enqueue(blocked_queue, running_thread);
-				temp->join_tid = running_thread->TID;//temp will use join_tid to figure which parent to unblock
-				uthread_yield();
-				break;
-			}
-			else{
-				return -1; 
-			}
-		}
-		curr_q = curr_q->next;
-	}
-
-		while(curr_blocked != NULL){
-			struct thread* temp_blocked = curr_blocked->key; //get the struct
-			if(temp_blocked->TID == tid){
-				found = 1;
-				if (temp_blocked->is_joined == false){
-			 		blocked = 1;
-                                	temp_blocked->is_joined = true;
-                                	running_thread->state = Blocked; //block the running thead.
-                                	queue_enqueue(blocked_queue, running_thread);
-                                	temp_blocked->join_tid = running_thread->TID;//temp will use join_tid to figure which parent to unblock
-                                	uthread_yield();
-					//delete child
-					break;	
-				}//if it hasnt been joined before
-				else{
-					return -1;
-				}
-			}
-			curr_blocked = curr_blocked->next;
-		}
-		while(curr_zombie != NULL){
-			struct thread* temp_zombie = curr_q->key;
-			if(temp_zombie->TID == tid){
-				found = 1;
-				if (temp_zombie->is_joined == false){
-					temp_zombie->is_joined = true;
-					if(retval != NULL)
-                                        	*retval = temp_zombie->retVal;
-					//free(curr_zombie);
-					//free(curr_q);
-					free(temp_zombie->context);
-					uthread_ctx_destroy_stack(temp_zombie->stack);
-					break;
-				}
-				else{
-					return -1;
-				}
-				//if(blocked == 1){
-					/*running_thread->state = Ready;
-					struct thread* running = running_thread;
-
-					queue_enqueue(q, running);
-					struct Node *top = q->front;
-        				struct thread* temp = (struct thread*)top->key;
-					//switch from your running thread tp next scheduled thread...
-        				if(queue_length(q) > 0){
-                				struct thread *curr;//= malloc(sizeof(thread));
-                				queue_dequeue(q,(void **)&curr);
-                				uthread_ctx_switch(running->context, temp->context); //what if there is nothing left in queue?
-        				}*/
-					uthread_yield();
-				//}
-				//removde temp_zombie from enqueue.. Deltete it!
-				break;
-			}
-			curr_zombie = curr_zombie->next;
-		}
-		/*while(1){
-			if(queue_length(q) == 0){
-				break;
-			}
-			uthread_yield();
-		}*/
-	if(found == 0){
-		return -1;
-	}
-		return 0;
+    while (curr_blocked != NULL) {
+        struct thread * temp_blocked = curr_blocked -> key; //get the struct
+        if (temp_blocked -> TID == tid) {
+            found = 1;
+            if (temp_blocked -> is_joined == false) {
+                blocked = 1;
+                temp_blocked -> is_joined = true;
+                running_thread -> state = Blocked; //block the running thead.
+                queue_enqueue(blocked_queue, running_thread);
+                temp_blocked -> join_tid = running_thread -> TID; //temp will use join_tid to figure which parent to unblock
+                uthread_yield();
+                //delete child
+                break;
+            } //if it hasnt been joined before
+            else {
+                return -1;
+            }
+        }
+        curr_blocked = curr_blocked -> next;
+    }
+    while (curr_zombie != NULL) {
+        struct thread * temp_zombie = curr_q -> key;
+        if (temp_zombie -> TID == tid) {
+            found = 1;
+            if (temp_zombie -> is_joined == false) {
+                temp_zombie -> is_joined = true;
+                if (retval != NULL)
+                    *
+                    retval = temp_zombie -> retVal;
+                free(temp_zombie -> context);
+                uthread_ctx_destroy_stack(temp_zombie -> stack);
+                break;
+            } else {
+                return -1;
+            }
+            uthread_yield();
+            break;
+        }
+        curr_zombie = curr_zombie -> next;
+    }
+    if (found == 0) {
+        return -1;
+    }
+    return 0;
 }
 
-int main_thread(uthread_func_t func, void *arg)
-{
-	struct thread *thread = malloc(sizeof(struct thread));
-	thread->TID = num_threads;
-        thread->state = Running; //assign to read?
-	thread->arg = arg;
-	thread->is_joined = false;
-	//thread->parent = NULL;
-	thread->func = func;
-	thread->context = malloc(sizeof(uthread_ctx_t));
-	void *top_of_stack = uthread_ctx_alloc_stack();
-        thread->stack = top_of_stack; //assign stack
-	int retVal = uthread_ctx_init(thread->context, top_of_stack, func, arg);
+int main_thread(uthread_func_t func, void * arg) {
+    struct thread * thread = malloc(sizeof(struct thread));
+    thread -> TID = num_threads;
+    thread -> state = Running; //assign to read?
+    thread -> arg = arg;
+    thread -> is_joined = false;
+    thread -> func = func;
+    thread -> context = malloc(sizeof(uthread_ctx_t));
+    void * top_of_stack = uthread_ctx_alloc_stack();
+    thread -> stack = top_of_stack; //assign stack
+    int retVal = uthread_ctx_init(thread -> context, top_of_stack, func, arg);
 
-	if (retVal == -1){
-		return -1;
-	}
-	num_threads++; //increment thread number
-	zombie_queue = queue_create();
-	blocked_queue = queue_create();
-	q = queue_create();
-	//printf("Queue created\n");	
-	running_thread = thread;
-	//printf("%d", running_thread->TID);
-	//queue_enqueue(q, thread); //add the struct to the queue
-	return 0; //return TID
+    if (retVal == -1) {
+        return -1;
+    }
+    num_threads++; //increment thread number
+    zombie_queue = queue_create();
+    blocked_queue = queue_create();
+    q = queue_create();
+    running_thread = thread;
+    return 0; //return TID
 }
-
