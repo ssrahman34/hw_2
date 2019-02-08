41d3819a0d67be35cfc5dfed0ad82ee00e2d8834
ipuri <ipuri@ad3.ucdavis.edu@pc23.cs.ucdavis.edu>
2019-02-08 00:02:41 -0800

    submit

--- p2/libuthread/preempt.c
+++ p2/libuthread/preempt.c
@@ -36,14 +36,13 @@ void preempt_enable(void)
 }
 void preempt_start(void)
 {
-        struct sigaction action;
-        sigemptyset (&action.sa_mask);
-        value.it_value.tv_usec = 10000;
-        value.it_value.tv_sec = 0;
-        value.it_interval.tv_sec = 0;
-        value.it_interval.tv_usec = 10000;
-        action.sa_handler = uthread_yield;//uthread_yield; //call yield fn
-        sigaction(SIGVTALRM,&action, NULL);
-
+    struct sigaction action;
+    sigemptyset (&action.sa_mask);
+    value.it_value.tv_usec = 10000;
+    value.it_value.tv_sec = 0;
+    value.it_interval.tv_sec = 0;
+    value.it_interval.tv_usec = 10000;
+    //action.sa_handler = handle;//uthread_yield; //call yield fn
+    sigaction(SIGVTALRM,&action, NULL);
 }
 //https://stackoverflow.com/questions/5316749/having-trouble-using-sigaction-with-a-timer-signal
--- p2/test/uthread_hello.d
+++ p2/test/uthread_hello.d
@@ -1,1 +1,4 @@
-uthread_hello.o: uthread_hello.c ../libuthread/uthread.h
+uthread_hello.o: uthread_hello.c ../libuthread/uthread.c \
+ ../libuthread/context.c ../libuthread/context.h ../libuthread/uthread.h \
+ ../libuthread/preempt.h ../libuthread/preempt.c ../libuthread/queue.h \
+ ../libuthread/queue.c ../libuthread/uthread.h
